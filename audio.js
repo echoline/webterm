ff47497e3f53778489e8445805dbd6e89dacf1d9
OpenBSD User <echoline@gmail.com>
2020-08-09 08:13:53 +0000

    "devaudio"

---
+++ audio.js
@@ -0,0 +1,53 @@
+mkfile("/dev/audio", function(f, p) {
+		try {
+			f.context = new (window.AudioContext || window.webkitAudioContext)();
+			f.queue = [];
+			f.buffer = "";
+		} catch(err) {
+			return err.message;
+		}
+	}, undefined,
+	function(f, p) {
+		try {
+			f.buffer += p.data;
+			if (f.buffer.length > 8192) {
+				f.context.decodeAudioData(str2arr(f.buffer).buffer, function(buffer) {
+					var source = f.context.createBufferSource();
+					source.buffer = buffer;
+					source.connect(f.context.destination);
+					source.onended = function() {
+						f.queue.shift();
+						if (f.queue.length == 0)
+							return;
+						f.queue[0].start();
+					}
+					f.queue.push(source);
+					if (f.queue.length == 1)
+						f.queue[0].start();
+				});
+				f.buffer = "";
+			}
+			respond(p, -1);
+		} catch(err) {
+			error9p(p.tag, err.message);
+		}
+	},
+	function(f, p) {
+		if (f.buffer.length > 0) {
+			f.context.decodeAudioData(str2arr(f.buffer).buffer, function(buffer) {
+				var source = f.context.createBufferSource();
+				source.buffer = buffer;
+				source.connect(f.context.destination);
+				f.queue.push(source);
+				if (f.queue.length == 1)
+					f.queue[0].start();
+			});
+			f.buffer = "";
+		}
+		if (f.queue.length > 0)
+			f.queue[f.queue.length-1].onended = function() {
+				f.context.close();
+			}
+		else
+			f.context.close();
+	});
